b44481221b6f986acd410305324e3ef4b103b818
Souvik Sen <souvik22sen@gmail.com>
2021-12-09 21:28:46 +0530

    Modified

--- Sorting/CountSort.cpp
+++ Sorting/CountSort.cpp
@@ -15,18 +15,31 @@ void countSort(vector<int> &arr)
 
     for (int i = 0; i < n; i++)
     {
-        count[arr[i]]++;
+        count[arr[i]]++; // stores the frequency of each element
     }
 
     for (int i = 1; i <= k; i++)
     {
         count[i] += count[i - 1];
     }
+
+    vector<int> output(n);
+
+    for (int i = n - 1; i >= 0; i--)
+    {
+        output[count[arr[i]] - 1] = arr[i];
+        count[arr[i]]--;
+        // OR output[--count[arr[i]]] = arr[i];
+    }
+
+    arr = output;
 }
 
 int main()
 {
     vector<int> arr = {1, 3, 2, 3, 4, 1, 6, 4, 3};
+
+    countSort(arr);
 
     for (int i = 0; i < arr.size(); i++)
     {
