4f665a0d2e110875c44da56b429b67421324ecb9
Souvik Sen <souvik22sen@gmail.com>
2021-12-14 01:53:22 +0530

    Modified

--- Sorting/CountSort.cpp
+++ Sorting/CountSort.cpp
@@ -1,50 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void countSort(vector<int> &arr)
-{
-    int n = arr.size();
-    int k = arr[0]; // k stores the value of maximum element.
-    // k = *max_element(arr.begin(), arr.end());
-    for (int i = 1; i < n; i++)
-    {
-        k = max(k, arr[i]);
-    }
+void countSort(vector<int> &arr) {
+  int n = arr.size();
+  int k = arr[0];  // k stores the value of maximum element.
+  // k = *max_element(arr.begin(), arr.end());
+  for (int i = 1; i < n; i++) {
+    k = max(k, arr[i]);
+  }
 
-    vector<int> count(k + 1, 0);
+  vector<int> count(k + 1, 0);
 
-    for (int i = 0; i < n; i++)
-    {
-        count[arr[i]]++; // stores the frequency of each element
-    }
+  for (int i = 0; i < n; i++) {
+    count[arr[i]]++;  // stores the frequency of each element
+  }
 
-    for (int i = 1; i <= k; i++)
-    {
-        count[i] += count[i - 1];
-    }
+  for (int i = 1; i <= k; i++) {
+    count[i] += count[i - 1];
+  }
 
-    vector<int> output(n);
+  vector<int> output(n);
 
-    for (int i = n - 1; i >= 0; i--)
-    {
-        output[count[arr[i]] - 1] = arr[i];
-        count[arr[i]]--;
-        // OR output[--count[arr[i]]] = arr[i];
-    }
+  for (int i = n - 1; i >= 0; i--) {
+    output[count[arr[i]] - 1] = arr[i];
+    count[arr[i]]--;
+    // OR output[--count[arr[i]]] = arr[i];
+  }
 
-    arr = output;
+  arr = output;
 }
 
-int main()
-{
-    vector<int> arr = {1, 3, 2, 3, 4, 1, 6, 4, 3};
+int main() {
+  vector<int> arr = {1, 3, 2, 3, 4, 1, 6, 4, 3};
 
-    countSort(arr);
+  countSort(arr);
 
-    for (int i = 0; i < arr.size(); i++)
-    {
-        cout << arr[i] << " ";
-    }
+  for (int i = 0; i < arr.size(); i++) {
+    cout << arr[i] << " ";
+  }
 
-    return 0;
+  return 0;
 }
--- Strings/Rearrange_to_form_palindrome.cpp
+++ Strings/Rearrange_to_form_palindrome.cpp
@@ -1,7 +1,7 @@
 /*
 
-Given a string, convert the string to palindrome without any 
-modifications like adding a character, removing a character, 
+Given a string, convert the string to palindrome without any
+modifications like adding a character, removing a character,
 replacing a character etc.
 Examples:
 
@@ -20,59 +20,51 @@ Output : "No Palindrome"
 #include <bits/stdc++.h>
 using namespace std;
 
-string getPalindrome(string str)
-{
+string getPalindrome(string str) {
+  // Store counts of characters
+  unordered_map<char, int> hmap;
+  for (int i = 0; i < str.length(); i++) hmap[str[i]]++;
 
-    // Store counts of characters
-    unordered_map<char, int> hmap;
-    for (int i = 0; i < str.length(); i++)
-        hmap[str[i]]++;
-
-    /* find the number of odd elements.
-	Takes O(n) */
-    int oddCount = 0;
-    char oddChar;
-    for (auto x : hmap)
-    {
-        if (x.second % 2 != 0)
-        {
-            oddCount++;
-            oddChar = x.first;
-        }
+  /* find the number of odd elements.
+      Takes O(n) */
+  int oddCount = 0;
+  char oddChar;
+  for (auto x : hmap) {
+    if (x.second % 2 != 0) {
+      oddCount++;
+      oddChar = x.first;
     }
+  }
 
-    /* odd_cnt = 1 only if the length of
-	str is odd */
-    if (oddCount > 1 || oddCount == 1 &&
-                            str.length() % 2 == 0)
-        return "NO PALINDROME";
+  /* odd_cnt = 1 only if the length of
+      str is odd */
+  if (oddCount > 1 || oddCount == 1 && str.length() % 2 == 0)
+    return "NO PALINDROME";
 
-    /* Generate first halh of palindrome */
-    string firstHalf = "", secondHalf = "";
-    // for (auto x : hmap)
-    //     cout << x.first << " " << x.second << endl;
-    for (auto x : hmap)
-    {
+  /* Generate first halh of palindrome */
+  string firstHalf = "", secondHalf = "";
+  // for (auto x : hmap)
+  //     cout << x.first << " " << x.second << endl;
+  for (auto x : hmap) {
+    // Build a string of floor(count/2)
+    // occurrences of current character
+    // cout << x.first << " " << x.second << endl;
+    string s(x.second / 2, x.first);
 
-        // Build a string of floor(count/2)
-        // occurrences of current character
-        //cout << x.first << " " << x.second << endl;
-        string s(x.second / 2, x.first);
+    // Attach the built string to end of
+    // and begin of second half
+    firstHalf = firstHalf + s;
+    secondHalf = s + secondHalf;
+  }
 
-        // Attach the built string to end of
-        // and begin of second half
-        firstHalf = firstHalf + s;
-        secondHalf = s + secondHalf;
-    }
-
-    // Insert odd character if there
-    // is any
-    return (oddCount == 1) ? (firstHalf + oddChar + secondHalf) : (firstHalf + secondHalf);
+  // Insert odd character if there
+  // is any
+  return (oddCount == 1) ? (firstHalf + oddChar + secondHalf)
+                         : (firstHalf + secondHalf);
 }
 
-int main()
-{
-    string s = "aabab";
-    cout << getPalindrome(s);
-    return 0;
+int main() {
+  string s = "aabab";
+  cout << getPalindrome(s);
+  return 0;
 }
