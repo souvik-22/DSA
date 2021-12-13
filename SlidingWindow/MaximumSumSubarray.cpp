b3fecda20ea190f6051e6bc3e3cdf8945d3eb384
Souvik Sen <souvik22sen@gmail.com>
2021-12-14 01:57:32 +0530

    Modified

--- SlidingWindow/MaximumSumSubarray.cpp
+++ SlidingWindow/MaximumSumSubarray.cpp
@@ -1,19 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int maximumSumSubarray(int k, vector<int> &arr)
-{
+int maximumSumSubarray(int k, vector<int>& arr) {
     int N = arr.size();
     int sum = 0;
     int i = 0, j = 0;
     int mx = INT_MIN;
-    while (j < N)
-    {
+    while (j < N) {
         sum += arr[j];
         if (j - i + 1 < k)
             j++;
-        else if (j - i + 1 == k)
-        {
+        else if (j - i + 1 == k) {
             mx = max(mx, sum);
             j++;
             sum -= arr[i];
@@ -23,8 +20,7 @@ int maximumSumSubarray(int k, vector<int> &arr)
     return mx;
 }
 
-int main()
-{
+int main() {
     int n, k;
     cin >> n >> k;
     vector<int> arr(n);
